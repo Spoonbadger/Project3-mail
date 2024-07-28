49fcada6dc847fc3213fd5b94527dcc7279b4e51
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-28 17:10:46 +0100

    Feat: counter added.

--- mail/templates/mail/alert.js
+++ mail/templates/mail/alert.js
@@ -1,23 +1,9 @@
 // Change title, change color of words in the title, click through the four subtitle options.
-function changeTitle() {
-    title = document.querySelector('#mainHeading')
-    if (title.innerHTML === "This is the main heading.") {
-        title.innerHTML = "And this is the second."
-    }
-    else if (title.innerHTML === "And this is the second.") {
-        title.innerHTML = "Final one I promise!"
-    }
-    else if (title.innerHTML === "Final one I promise!") {
-        title.innerHTML = "Just kidding ;p"
-    }
-    else title.innerHTML = "This is the main heading."
-}
-
-
 document.addEventListener('DOMContentLoaded', () => {
     // Set variables
-
-    subtitle = document.querySelector('#subtitle')
+    let title = document.querySelector('#mainHeading')
+    let subtitle = document.querySelector('#subtitle')
+    let counter = 0;
 
     // change title
     document.querySelector('#mainTitleButton').onclick = changeTitle;
@@ -30,4 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     // Change the subtitle
+    document.querySelector('#subtitleSelect').onchange = changeSubtitle;
+
+    function changeTitle() {
+        if (title.innerHTML === "This is the main heading.") {
+            title.innerHTML = "And this is the second.";
+        }
+        else if (title.innerHTML === "And this is the second.") {
+            title.innerHTML = "Final one I promise!";
+        }
+        else if (title.innerHTML === "Final one I promise!") {
+            title.innerHTML = "Just kidding ;p";
+        }
+        else title.innerHTML = "This is the main heading."
+    }
+    
+    function changeSubtitle(event) {
+        subtitle.innerHTML = event.target.value;
+    }
+
+
+    // Counter
+    document.querySelector('#counterButton').onclick = count;
+
+    function count() {
+        counter ++;
+        document.querySelector('#counter').innerHTML = counter;
+    }
 });
+
