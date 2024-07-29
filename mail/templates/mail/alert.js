cbf5b45a8a87f90b488a513bd683a2b2f9f05840
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-29 13:08:52 +0100

    Feat: title changes to random color with each change.

--- mail/templates/mail/alert.js
+++ mail/templates/mail/alert.js
@@ -4,5 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function changeTitle() {
         let newHeading = document.querySelector('#inputBox').value;
         document.querySelector('#titleMain').innerHTML = newHeading;
+
+        let colors = ['green', 'darkgreen', 'pink', 'darkpink', 'blue', 'darkblue', 'orange', 'red', 'darkred', 'yellow'];
+        let randomColor = colors[Math.floor(Math.random() * colors.length)];
+        document.querySelector('#titleMain').style.color = randomColor;
     }
 });
