426d27392ccf31db19c64c1d67467d616c80167d
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-29 13:01:31 +0100

    Feat: change heading textbox.

--- mail/templates/mail/alert.js
+++ mail/templates/mail/alert.js
@@ -1,47 +1,8 @@
-// Change title, change color of words in the title, click through the four subtitle options.
 document.addEventListener('DOMContentLoaded', () => {
-    // Set variables
-    let title = document.querySelector('#mainHeading')
-    let subtitle = document.querySelector('#subtitle')
-    let counter = 0;
-
-    // change title
-    document.querySelector('#mainTitleButton').onclick = changeTitle;
-
-    // Change title color
-    document.querySelectorAll('button[data-color]').forEach((button) => {
-        button.onclick = () => {
-            title.style.color = button.dataset.color;
-        }
-    })
-
-    // Change the subtitle
-    document.querySelector('#subtitleSelect').onchange = changeSubtitle;
+    document.querySelector('#headingButton').onclick = changeTitle;
 
     function changeTitle() {
-        if (title.innerHTML === "This is the main heading.") {
-            title.innerHTML = "And this is the second.";
-        }
-        else if (title.innerHTML === "And this is the second.") {
-            title.innerHTML = "Final one I promise!";
-        }
-        else if (title.innerHTML === "Final one I promise!") {
-            title.innerHTML = "Just kidding ;p";
-        }
-        else title.innerHTML = "This is the main heading."
-    }
-    
-    function changeSubtitle(event) {
-        subtitle.innerHTML = event.target.value;
-    }
-
-
-    // Counter
-    document.querySelector('#counterButton').onclick = count;
-
-    function count() {
-        counter ++;
-        document.querySelector('#counter').innerHTML = counter;
+        let newHeading = document.querySelector('#inputBox').value;
+        document.querySelector('#titleMain').innerHTML = newHeading;
     }
 });
-
