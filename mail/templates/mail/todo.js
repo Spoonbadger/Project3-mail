08e5a2e99ed655695d97a878bd26b82a0594bbc5
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-30 13:11:48 +0100

    Feat: automatica timer with manual addition button.

--- mail/templates/mail/todo.js
+++ mail/templates/mail/todo.js
@@ -1,36 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Append task items to a webpage list
-    let newTask = document.querySelector('#taskInput')
+    // Set variables
+    counter = 0;
+    counterElement = document.querySelector('#counterElement');
+    hasStarted = false;
+    
+    document.querySelector('#counterButton').onclick = () => {
+        counter ++;
+        counterElement.innerHTML = counter;
 
-    document.querySelector('#taskForm').onsubmit = () => {
-        let li = document.createElement('li');
-        li.innerHTML = newTask.value;
-        // Add a button to each task item which can remove each task item...
-        removeButton = document.createElement('button')
-        removeButton.innerHTML = "remove"
-        li.appendChild(removeButton);
-
-        document.querySelector('#tasks').appendChild(li);
-
-        // Have the button remove item on click
-        removeButton.onclick = () => {
-            li.remove()
-        }
-        // Reset the form
-        newTask.value = '';
-        submit.disabled = true;
-        // Stop the form from submitting
-        return false;
-    }
-    // Disable the submit button when there is no text
-    submit = document.querySelector('#submitButton');
-    submit.disabled = true;
-    newTask.onkeyup = () => {
-        if (newTask.value.length > 0) {
-            submit.disabled = false;
-        }
-        else {
-            submit.disabled = true;
+        if (!hasStarted) {
+            hasStarted = true;
+            setInterval(() => {
+                counter ++;
+                counterElement.innerHTML = counter;
+            }, 1000)
         }
     }
 });
