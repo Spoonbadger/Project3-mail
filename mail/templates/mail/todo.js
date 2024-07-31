4a157dd16cfd066ad1b65492e557a0a5b92cf2b9
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-31 10:33:34 +0100

    Autocounter with click to add and localStorage saved.

--- mail/templates/mail/todo.js
+++ mail/templates/mail/todo.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Set variables
-    counter = 0;
-    counterElement = document.querySelector('#counterElement');
-    hasStarted = false;
-    
-    document.querySelector('#counterButton').onclick = () => {
-        counter ++;
-        counterElement.innerHTML = counter;
+    // Check if there is already a value in local storage
+    if (!localStorage.getItem('counter')) {
+        // If not, set the counter to 0 in local storage
+        localStorage.setItem('counter', 0);
+    }
 
-        if (!hasStarted) {
-            hasStarted = true;
+    // Retrieve counter value from local storage
+    let counter = parseInt(localStorage.getItem('counter'));
+    let hasBegun = false;
+    
+    function count() {
+        // Increment counter
+        counter++;
+        document.querySelector('#counterElement').innerHTML = counter;
+        
+        // Store updated counter in local storage
+        localStorage.setItem('counter', counter);
+        
+        // Begin automatic counter once upon first click
+        if (!hasBegun) {
             setInterval(() => {
-                counter ++;
-                counterElement.innerHTML = counter;
-            }, 1000)
+                counter++;
+                document.querySelector('#counterElement').innerHTML = counter;
+                localStorage.setItem('counter', counter);
+            }, 1000);
+            hasBegun = true;
         }
     }
+
+    // Set heading to the current value inside local storage
+    document.querySelector('#counterElement').innerHTML = counter;
+    document.querySelector('#counterButton').onclick = count;
 });
