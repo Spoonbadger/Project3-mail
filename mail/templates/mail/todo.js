c165ea3b3de747d09bbcd4f337260c9a6ea31c3c
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-31 11:07:26 +0100

    Feat: localStorage int items parsed specifically as ints.

--- mail/templates/mail/todo.js
+++ mail/templates/mail/todo.js
@@ -1,34 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Check if there is already a value in local storage
+    // Check if there is already a 'counter' value in localStorage, if not, set it to 0
     if (!localStorage.getItem('counter')) {
-        // If not, set the counter to 0 in local storage
         localStorage.setItem('counter', 0);
     }
 
-    // Retrieve counter value from local storage
-    let counter = parseInt(localStorage.getItem('counter'));
-    let hasBegun = false;
-    
-    function count() {
-        // Increment counter
-        counter++;
-        document.querySelector('#counterElement').innerHTML = counter;
-        
-        // Store updated counter in local storage
-        localStorage.setItem('counter', counter);
-        
-        // Begin automatic counter once upon first click
-        if (!hasBegun) {
-            setInterval(() => {
-                counter++;
-                document.querySelector('#counterElement').innerHTML = counter;
-                localStorage.setItem('counter', counter);
-            }, 1000);
-            hasBegun = true;
-        }
-    }
+    // Ensure the inital value on the webpage is set to the current value of counter in the localStorage
+    document.querySelector('#counterElement').innerHTML = localStorage.getItem('counter');
 
-    // Set heading to the current value inside local storage
-    document.querySelector('#counterElement').innerHTML = counter;
-    document.querySelector('#counterButton').onclick = count;
+    // When the button is clicked the counter should increase by a value of 1
+    document.querySelector('#counterButton').onclick = () => {
+        let counter = parseInt(localStorage.getItem('counter'));
+        counter ++;
+        document.querySelector('#counterElement').innerHTML = counter;
+        localStorage.setItem('counter', counter);
+    }
 });
+
