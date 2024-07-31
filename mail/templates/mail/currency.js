fb62502c417b04d42c1369a00127f01ae39b81e8
Spoonbadger <craig.adam.morley@gmail.com>
2024-07-31 16:03:03 +0100

    Feat: currency conversion.

---
+++ mail/templates/mail/currency.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Submit form
+    document.querySelector('#currencyForm').onsubmit = (event) => {
+        // Stop the form default submitting
+        event.preventDefault();
+        
+        // GET the exchange data
+        fetch('http://api.exchangeratesapi.io/v1/latest?access_key=9675e1aa6ca84ae4a0bf6071c04b4abd&symbol=USD,JPY,MXN,GBP,CAD,AUD')
+        .then(response => response.json())
+        .then(data => {
+            // Get the currency the user has input
+            let currency = document.querySelector('#currencyInput').value.toUpperCase();
+            // Get the relevant exchange rate from the data
+            let exchangeRate = data.rates[currency];
+            // Print out on the webpage
+            document.querySelector('#currencyParagraph').innerHTML = `$1 = ${exchangeRate.toFixed(3)} ${currency}`;
+        })
+    }
+})
