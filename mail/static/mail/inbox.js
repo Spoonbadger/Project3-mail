4009a8d2236839413c9a6510b28a50d97db85ab6
Spoonbadger <craig.adam.morley@gmail.com>
2024-08-06 16:22:57 +0100

    Feat: Add body contents to reply email.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -151,7 +151,7 @@ function read_email(email, user) {
           else {
             document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
           }
-          document.querySelector('#compose-body').value = `On ${email.timestamp}, ${email.sender} wrote:\n`;
+          document.querySelector('#compose-body').value = `On ${email.timestamp}, ${email.sender} wrote:\n ${email.body}\n`;
         }
       }
   });
